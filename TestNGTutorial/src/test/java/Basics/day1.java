88ed3cf96cfa8e99f9a322923f9ce7e96420422a
ravimuduthana <ravimuduthana@gmail.com>
2021-09-08 21:29:20 +0530

    changes

---
+++ TestNGTutorial/src/test/java/Basics/day1.java
@@ -0,0 +1,20 @@
+package Basics;
+
+import org.testng.annotations.Test;
+
+public class day1 
+{
+	@Test
+	public void Demo()
+	{
+		System.out.println("hello");
+	}
+	
+	@Test
+	public void SecondTest()
+	{
+		System.out.println("Bye");
+	}
+
+
+}
